1a3d401a1d62648a97aa3d40b1510b3ce0939943
Kaushik Bhattacharya <mailtokaushik1@gmail.com>
2020-08-16 19:07:29 +0530

    second commit

---
+++ src/app/audits/audit/audit.component.ts
@@ -0,0 +1,39 @@
+import { ClientFirm } from './../../shared/clientFirm.model';
+import { AuditService } from './audit.service';
+import { Component, OnInit } from '@angular/core';
+
+import { NgForm } from '@angular/forms';
+import { ConsoleReporter } from 'jasmine';
+
+@Component({
+  selector: 'app-audit',
+  templateUrl: './audit.component.html',
+  styleUrls: ['./audit.component.css']
+})
+export class AuditComponent implements OnInit {
+  clientFirms : ClientFirm[];
+  constructor(private auditService: AuditService) {
+    this.clientFirms = auditService.getClientFirms();
+     console.log(this.clientFirms);
+   }
+
+  ngOnInit(): void {
+    this.auditService.formData = {
+      AuditCode:'0',
+      Description:'',
+      AuditorFirm:'',
+      ClientFirm:'',
+      ReleaseDate:''
+    }
+  }
+
+  SaveAudit(form: NgForm)
+  {
+    this.auditService.Create().subscribe(response=>{
+      //form.form.reset;
+    });
+    console.log(form);
+  }
+
+}
+
